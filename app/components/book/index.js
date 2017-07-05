6dc8993a1d4ffd0af776340e6c680c6b80f0d928
Xu Jin <devxu@CNdevxu.local>
2017-07-05 10:03:50 +0800

    modify index.js

--- app/components/book/index.js
+++ app/components/book/index.js
@@ -39,11 +39,40 @@ export class bookContent extends React.Component{
             <ul className="list_wrap">
                 {
                     this.props.list.map((item, index) => {
-                        console.log(item);
-
                         return 
-                            <bookItem key={index} content={item} />
-                        
+                            (
+                                <li key={index} className="clearfix">
+                                    <img src={item.image} className="left"/>
+                                    <div className="right intro">
+                                        <p>
+                                            <label>{ '书名: ' }</label> 
+                                            <span>{item.title}</span>
+                                        </p>
+                                        <p>
+                                            <label>{ '作者: ' }</label>
+                                            <span>
+                                                {
+                                                    item.author.map((elem) => {
+                                                        return elem
+                                                    })
+                                                }
+                                            </span>
+                                        </p>
+                                        <p>
+                                            <label>{ '标签: ' }</label>
+                                            <span>
+                                                { '暂无' }
+                                            </span>
+                                        </p>
+                                        <p className="summary">
+                                            <label>{ '简介: ' }</label>
+                                            <span>
+                                                {item.summary}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </li>
+                            )  
                     })
                 }
             </ul>
